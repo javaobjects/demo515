d99624b191e678ade9cd83c1154b50160d95d10c
yanbo0039 <yanbo0039@yeah.net>
2019-07-09 16:37:28 +0800

    什么是异常

--- src/异常的处理/Test.java
+++ src/异常的处理/Test.java
@@ -3,6 +3,16 @@ package 异常的处理;
 public class Test {
 
 	public static void main(String[] args) {
+		
+//		double p = "";//这是语法错误，称不上异常
+		/**
+		 * 异常：
+		 * 代码运行期间出现的错误
+		 * 打开一个不存在的文件
+		 * 网络连接中断
+		 * 操作数组越界等
+		 */
+		
 		for (int i = 0; i < 10; i++) {
 			System.out.println(i);
 		}
