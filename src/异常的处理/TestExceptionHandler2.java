05657a7cd7a625cf5c24d2bdb4e753a5a0113d1a
yanbo0039 <yanbo0039@yeah.net>
2019-07-09 19:20:40 +0800

    finally语句块的作用&final_finalize_finally的区别

--- src/异常的处理/TestExceptionHandler2.java
+++ src/异常的处理/TestExceptionHandler2.java
@@ -12,10 +12,12 @@ public class TestExceptionHandler2 {
 		File file = new File("e:\\HelloWorld.java");
 		
 		
-		FileInputStream is;
+		FileInputStream is = null;
 		try {
 			is = new FileInputStream(file);
 			int result = is.read();//根据提示选择clause to surrounding by 
+			
+			
 			Class.forName("java.a.b");
 
 		} catch (FileNotFoundException e) {
@@ -25,12 +27,25 @@ public class TestExceptionHandler2 {
 			e1.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
+//1.finally语句块有什么 作用？
+//	用于释放资源，即使代码抛出异常，
+//	finally语句块中的代码也会执行
+		}finally {
+			try {
+				is.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
-		
-
-		
-	
-	
+//2.final finalize finally 有什么区别？
+/**
+ * final 修饰的类不能有子类 
+ *       修饰的方法不能重写 
+ *       修饰的变量不能重新赋值，叫常量
+ * finalize：如果发现有垃圾对象的话就会调用finalize()方法，释放它所占用的内存 
+ * finally: 确保我们程序当中有些释放资源的方法能够执行，
+ * 			一般跟在try catch语句块后面     	
+ */
 
 	}
 
